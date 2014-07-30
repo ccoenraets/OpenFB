91ed80e95a1264c2d73a0a7f3834fbc06ff3db1b
Gordon Sun <sunshineo@gmail.com>
2014-07-29 23:31:57 -0700

    Add google.

--- openfb.js
+++ openfb.js
@@ -370,3 +370,205 @@ var createFB = function () {
 
 };
 var openFB = createFB();
+
+
+
+/**
+ * OpenGoogle: Try build on top of OpenOAuth
+ */
+var createGoogle = function () {
+
+    var GOOGLE_LOGIN_URL = 'https://accounts.google.com/o/oauth2/auth',
+        GOOGLE_LOGOUT_URL = 'https://accounts.google.com/logout',
+        GOOGLE_API_URL = 'https://www.googleapis.com/',
+
+        googleAppId,
+
+        context = window.location.pathname.substring(0, window.location.pathname.indexOf("/",2)),
+
+        baseURL = location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '') + context,
+
+	// Moved calculate of these two values into init function
+	// Requires call init function after device ready to calculate correctly
+        oauthRedirectURL = '',
+        logoutRedirectURL = '',
+
+        // Because the OAuth login spans multiple processes, we need to keep the login callback function as a variable
+        // inside the module instead of keeping it local within the login function.
+        loginCallback,
+
+        // Used in the exit event handler to identify if the login has already been processed elsewhere (in the oauthCallback function)
+        loginProcessed,
+
+        // Store loginUrl calculated when init and used when login is called.
+	loginUrl,
+
+        // Gradually move things here to support other services in the future
+        openOAuth;
+
+    /**
+     * Initialize the OpenGoogle module. You must use this function and initialize the module with an appId before you can
+     * use any other function.
+     * @param params - init paramters
+     *  appId: The id of the Facebook app,
+     *  tokenStore: The store used to save the Facebook token. Optional. If not provided, we use sessionStorage.
+     */
+    function init(params) {
+        if (params.appId) {
+            googleAppId = params.appId;
+        } else {
+            throw 'appId parameter not set in init()';
+        }
+
+	// phonegap is for old version support
+	var runningInCordova = !!window.cordova || !!window.phonegap;
+
+        if (runningInCordova) {
+	    // Login works with pretty much anything such as http://localhost
+            oauthRedirectURL = "http://localhost/index.html";
+	    // This is the only url I found works for logout when you do not have your own server
+            logoutRedirectURL = "http://localhost/index.html";
+        }
+	else {
+            oauthRedirectURL = baseURL + '/oauthcallback.html';
+            logoutRedirectURL = baseURL + '/logoutcallback.html';
+	}
+	if (!params.loginScope) {
+            params['loginScope'] = "profile email";
+	}
+        loginUrl = GOOGLE_LOGIN_URL + '?client_id=' + googleAppId + '&redirect_uri=' + oauthRedirectURL +
+            '&response_type=token&scope=' + params.loginScope;
+	console.log(loginUrl);
+
+	if (!params.tokenKey) {
+            params['tokenKey'] = 'googletoken';
+        }
+        openOAuth = createOpenOAuth(params);
+
+    }
+
+    /**
+     * Checks if the user has logged in with openFB and currently has a session api token.
+     * @param callback the function that receives the loginstatus
+     */
+    function getLoginStatus(callback) {
+        openOAuth.getLoginStatus(callback);
+    }
+
+    /**
+     * Login to Facebook using OAuth. If running in a Browser, the OAuth workflow happens in a a popup window.
+     * If running in Cordova container, it happens using the In-App Browser. Don't forget to install the In-App Browser
+     * plugin in your Cordova project: cordova plugins add org.apache.cordova.inappbrowser.
+     *
+     * @param callback - Callback function to invoke when the login process succeeds
+     * @param options - options.scope: The set of Facebook permissions requested
+     * @returns {*}
+     */
+    function login(callback, options) {
+        if (!googleAppId) {
+            return callback({status: 'unknown', error: 'Facebook App Id not set.'});
+        }
+
+        var loginUrlTemp;
+        // Re-calculate loginUrl if specified scope
+        if (options && options.loginScope) {
+            loginUrlTemp = GOOGLE_LOGIN_URL + '?client_id=' + fbAppId + '&redirect_uri=' + oauthRedirectURL +
+                '&response_type=token&scope=' + params.loginScope;
+        } else {
+            loginUrlTemp = loginUrl;
+        }
+
+        loginCallback = callback;
+        loginProcessed = false;
+
+	openOAuth.login(loginUrlTemp, oauthCallback);
+    }
+
+
+    /**
+     * Called either by oauthcallback.html (when the app is running the browser) or by the loginWindow loadstart event
+     * handler defined in the login() function (when the app is running in the Cordova/PhoneGap container).
+     * @param url - The oautchRedictURL called by Facebook with the access_token in the querystring at the ned of the
+     * OAuth workflow.
+     */
+    function oauthCallback(url) {
+        // Parse the OAuth data received from Facebook
+        var queryString,
+            obj;
+
+        loginProcessed = true;
+        if (url.indexOf("access_token=") > 0) {
+            queryString = url.substr(url.indexOf('#') + 1);
+            obj = parseQueryString(queryString);
+            openOAuth.setToken(obj['access_token']);
+            if (loginCallback) loginCallback({status: 'connected', authResponse: {token: obj['access_token']}});
+        } else if (url.indexOf("error=") > 0) {
+            queryString = url.substring(url.indexOf('?') + 1, url.indexOf('#'));
+            obj = parseQueryString(queryString);
+            if (loginCallback) loginCallback({status: 'not_authorized', error: obj.error});
+        } else {
+            if (loginCallback) loginCallback({status: 'not_authorized'});
+        }
+    }
+
+    /**
+     * Logout from Facebook, and remove the token.
+     * IMPORTANT: For the Facebook logout to work, the logoutRedirectURL must be on the domain specified in "Site URL" in your Facebook App Settings
+     *
+     */
+    function logout(callback) {
+        var logoutUrl = GOOGLE_LOGOUT_URL + '?access_token=' + openOAuth.getToken() + '&next=' + logoutRedirectURL;
+
+	openOAuth.logout(logoutUrl, callback);
+    }
+
+    /**
+     * Lets you make any Facebook Graph API request.
+     * @param obj - Request configuration object. Can include:
+     *  method:  HTTP method: GET, POST, etc. Optional - Default is 'GET'
+     *  path:    path in the Facebook graph: /me, /me.friends, etc. - Required
+     *  params:  queryString parameters as a map - Optional
+     *  success: callback function when operation succeeds - Optional
+     *  error:   callback function when operation fails - Optional
+     */
+    function api(obj) {
+        var params = obj.params || {};
+        params['access_token'] = openOAuth.getToken();
+        obj['url'] = FB_API_URL + obj.path + '?' + toQueryString(params);
+
+        openOAuth.api(obj);
+    }
+
+    function toQueryString(obj) {
+        var parts = [];
+        for (var i in obj) {
+            if (obj.hasOwnProperty(i)) {
+                parts.push(encodeURIComponent(i) + "=" + encodeURIComponent(obj[i]));
+            }
+        }
+        return parts.join("&");
+    }
+
+    function parseQueryString(queryString) {
+        var qs = decodeURIComponent(queryString),
+            obj = {},
+            params = qs.split('&');
+        params.forEach(function (param) {
+            var splitter = param.split('=');
+            obj[splitter[0]] = splitter[1];
+        });
+        return obj;
+    }
+
+    // The public API
+    return {
+        init: init,
+        login: login,
+        logout: logout,
+        api: api,
+        oauthCallback: oauthCallback,
+        getLoginStatus: getLoginStatus
+    }
+
+};
+var openGoogle = createGoogle();
