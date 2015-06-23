5c250f508cb2e6a95ccbb0283a1886b70d0f1cb2
Adrian Miranda <adriancmiranda@gmail.com>
2015-06-23 00:04:36 -0300

    Update openfb.js

--- openfb.js
+++ openfb.js
@@ -300,6 +300,28 @@ var openFB = (function () {
 		return parts.join('&');
 	}
 	
+	function utf8Decode(str_data){
+		var tmp_arr = [], i = 0, ac = 0, c1 = 0, c2 = 0, c3 = 0;
+		str_data += '';
+		while (i < str_data.length) {
+			c1 = str_data.charCodeAt(i);
+			if(c1 < 128){
+				tmp_arr[ac++] = String.fromCharCode(c1);
+				i++;
+			}else if (c1 > 191 && c1 < 224){
+				c2 = str_data.charCodeAt(i + 1);
+				tmp_arr[ac++] = String.fromCharCode(((c1 & 31) << 6) | (c2 & 63));
+				i += 2;
+			}else{
+				c2 = str_data.charCodeAt(i + 1);
+				c3 = str_data.charCodeAt(i + 2);
+				tmp_arr[ac++] = String.fromCharCode(((c1 & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+				i += 3;
+			}
+		}
+		return tmp_arr.join('');
+	}
+	
 	function base64Decode(data){
 		var b64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
 		var o1, o2, o3, h1, h2, h3, h4, bits, i = 0, ac = 0, dec = '', tmp_arr = [];
@@ -325,7 +347,7 @@ var openFB = (function () {
 			}
 		} while(i < data.length);
 		dec = tmp_arr.join('');
-		dec = this.utf8Decode(dec);
+		dec = utf8Decode(dec);
 		return dec;
 	}
 	
